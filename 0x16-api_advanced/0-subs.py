f0aeb95c66054925ddd9b4c0e40dfd315cb5b853
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 18:00:05 +0000

    subscribe

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,33 +6,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets number of subscribers
-       Args:
-           subreddit (str): name of subreddit
-       Returns:
-           number of subscribers if valid, 0 otherwise
-    """
-    base_url = 'https://api.reddit.com/r/'
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    response = requests.get(
-        '{}{}/about'.format(
-            base_url, subreddit), headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        return 0
-
-    try:
-        about_dict = response.json()
-        return about_dict['data']['subscribers']
-    except (ValueError, KeyError):
-        return 0
-
-
-if __name__ == '__main__':
-    import sys
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    """Return number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+        return 0

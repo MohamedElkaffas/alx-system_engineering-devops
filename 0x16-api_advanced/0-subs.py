503a0987193d7ac0013826e086e914270ef043dc
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 18:40:16 +0000

    subscr

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 
-n that returns the number of subscribers for a given subreddit"""
+"""dunctionn that returns # of subscribers for a given subreddit"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Gets number of subscribers
@@ -9,24 +11,15 @@ def number_of_subscribers(subreddit):
        Returns:
            number of subscribers if valid, 0 otherwise
     """
-    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'custom-user-agent'}
-    
-    try:
-        # Fetch the subreddit info
-        response = requests.get(base_url, headers=headers, allow_redirects=False)
-        
-        # If the response code is not 200, it's either not found or another issue
-        if response.status_code != 200:
-            return 0
-        
-        # Load the JSON response
-        data = response.json()
-        
-        # Extract the number of subscribers
-        return data.get('data', {}).get('subscribers', 0)
-    
-    except (requests.RequestException, ValueError, KeyError):
-        # Return 0 in case of any network error or invalid JSON structure
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/about'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
 
+    about_dict = response.json()
+
+    return about_dict['data']['subscribers']

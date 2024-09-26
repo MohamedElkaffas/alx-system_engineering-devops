bcd8d1d44320332beb670f48bdd52fb6877565ec
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 22:56:28 +0000

    g

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""function that returns the number of subscribers for a given subreddit"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets number of subscribers
-       Args:
-           subreddit (str): name of subreddit
-       Returns:
-           number of subscribers if valid, 0 otherwise
-    """
-    base_url = 'https://api.reddit.com/r/'
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    response = requests.get(
-        '{}{}/about'.format(
-            base_url, subreddit), headers=headers, allow_redirects=False)
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    print(f"Response status code: {response.status_code}")  # Debugging line
     if response.status_code != 200:
+        print("Returning 0 due to invalid subreddit or other error")  # Debugging line
         return 0
-
-    about_dict = response.json()
-
-    return about_dict['data']['subscribers']
+    data = response.json().get("data")
+    if data is None:
+        print("Returning 0 due to missing data in response")  # Debugging line
+        return 0
+    num_subs = data.get("subscribers", 0)
+    print(f"Number of subscribers: {num_subs}")  # Debugging line
+    return num_subs

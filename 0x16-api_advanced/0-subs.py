d1223ab6d8796e693bb0021ae9ba1e1401373a2e
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 17:41:42 +0000

    subscribe

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Uses reddit API to print #  of subscribers of a subreddit
+"""
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """Get subscribers count by subreddit given"""
+    url_sred_inf = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_sred_inf, headers=headers,
+                            allow_redirects=0)
+    if str(response) != "<Response [200]>":
+        return 0
+    r_json = response.json()
+    num_subs = r_json.get("data").get("subscribers")
+    return num_subs

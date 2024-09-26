94a7748cdeac59b48899c2f5724b58e1b35034cb
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 19:33:45 +0000

    uaaarab

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-
+"""This module makes a request to the reddit api"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    """gets number of subscribers of a subreddit"""
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+    }
+    try:
+        res = requests.get('https://www.reddit.com/r/'+subreddit
+                           + '/hot.json?limit=10', headers=headers,
+                           allow_redirects=False)
+        subreddit_data = res.json()
+    except Exception:
+        print('None')
+    if 'error' in subreddit_data.keys():
+        print('None')
     else:
-        print(None)
+        details = subreddit_data['data']['children']
+        for detail in details:
+            print(detail['data']['title'])

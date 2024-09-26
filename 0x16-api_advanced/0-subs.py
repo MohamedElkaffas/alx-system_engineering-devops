dd5a49e00615a21d6a6c566ee4c402cb675719ed
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 22:59:48 +0000

    x

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,17 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/izzyofc)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    print(f"Response status code: {response.status_code}")  # Debugging line
+    print("Response status code: {}".format(response.status_code))
     if response.status_code != 200:
-        print("Returning 0 due to invalid subreddit or other error")  # Debugging line
+        print("Returning 0 due to invalid subreddit or other error")
         return 0
     data = response.json().get("data")
     if data is None:
-        print("Returning 0 due to missing data in response")  # Debugging line
+        print("Returning 0 due to missing data in response")
         return 0
     num_subs = data.get("subscribers", 0)
-    print(f"Number of subscribers: {num_subs}")  # Debugging line
+    print("Number of subscribers: {}".format(num_subs))
     return num_subs

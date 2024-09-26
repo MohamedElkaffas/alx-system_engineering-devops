f0e8bc599eada8aefdcc2180895f7f7ffc10fed5
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 18:59:08 +0000

    ss

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,25 @@
-mport requests
+#!/usr/bin/python3
+"""Function that prints top ten hot posts for a given subreddit"""
+import requests
+
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Custom"}
-    
-    try:
-        req = requests.get(url, headers=headers, params={"limit": 10})
-        req.raise_for_status()
-        
-        if req.status_code == 200 and req.text:
-            data = req.json()
-            posts = data.get("data", {}).get("children", [])
-            for post in posts:
-                title = post.get("data", {}).get("title")
-                print(title)
-        else:
-            print(None)
-    except requests.exceptions.HTTPError:
-        print(None)
-    except ValueError:
-        print(None)
+    """Gets top ten posts in subreddit
+       Args:
+           subreddit (str): name of subreddit
+    """
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/hot?limit=10'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
 
+    if response.status_code != 200:
+        print('None')
+    else:
+        hot_dict = response.json()
+        if len(hot_dict['data']['children']) == 0:
+            print('None')
+        else:
+            for d in hot_dict['data']['children']:
+                print(d['data']['title'])

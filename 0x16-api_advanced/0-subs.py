2b4c8b6f824c85f2060094087bd97789c4eb88ec
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 22:45:05 +0000

    yarab

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,9 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/izzyofc)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
     data = response.json().get("data")
     if data is None:
         return 0
-    return data.get("subscribers", 0)
+    return data.get("subscribers")

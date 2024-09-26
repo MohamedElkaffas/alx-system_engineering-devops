0c391a9f414aad4e6b643d43dbb4576ae4f96de5
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-26 17:43:51 +0000

    subscribee

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url_sred_inf = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'request'}
     response = requests.get(url_sred_inf, headers=headers,
-                            allow_redirects=0)
+                            allow_redirects=False)
     if str(response) != "<Response [200]>":
         return 0
     r_json = response.json()

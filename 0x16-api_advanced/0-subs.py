bd6f351cd73b70c189c8b22c8ed7158d5091abdf
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2024-09-27 00:39:33 +0000

    a7ada3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,25 +2,20 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "python:subreddit.subscriber.counter:v2.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 403:
-        print("Access forbidden. Check User-Agent string or rate limiting.")
+    username = 'Bubbly-Mud1048'
+    password = 'tempa7ad@3'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/Bubbly-Mud1048)'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
+    client = requests.session()
+    client.headers = headers
+    client.post('https://www.reddit.com/api/login', data=user_pass_dict)
+    
+    response = client.get(url, allow_redirects=False)
+    if response.status_code != 404:
+        return response.json().get("data", {}).get("subscribers", 0)
+    else:
         return 0
-    if response.status_code != 200:
-        return 0
-    try:
-        results = response.json().get("data")
-    except ValueError:
-        print("Invalid JSON response")
-        return 0
-    if results is None:
-        return 0
-    return results.get("subscribers", 0)
-
